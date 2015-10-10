f22f501b3f0ee99a10ba0f7c24117f9284036d37
Eduardo Ramírez Farias <eedmolteen@gmail.com>
2015-10-10 02:35:56 -0300

    la wea es name

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   has_many :clases
   has_secure_password
   validates :email,    presence: true, uniqueness:  true
-  validates :nombre,     presence: true
+  validates :name,     presence: true
   validates :password, presence: true, length: { minimum: 6 }
   attr_accessor :remember_token
 
